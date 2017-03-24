56adf4c28c7e22bfdb0c4531e532d44fb4ef630e
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-24 15:12:07 +0800

    add homepage redirect button on display page

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -9,6 +9,6 @@ require('capybara/rspec')
       fill_in('sentence', :with => "The dog was walking on the street while I was passing by eating a hot-dog.")
       fill_in('word', :with => "dog")
       click_button('Go')
-      expect(page).to have_content("The word 'dog' is repeated 1 time(s) in the sentence 'The dog was walking on the street while I was passing by eating a hot-dog.' and records a partial match 2 time(s).")
+      expect(page).to have_content("The word \"dog\" is repeated 1 time(s) in the sentence \"The dog was walking on the street while I was passing by eating a hot-dog.\" and records a partial match 2 time(s).")
     end
   end
