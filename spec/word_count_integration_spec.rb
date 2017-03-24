2c6204ba114cc3eef230cebe5b2d8221876d1d22
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-24 10:50:00 +0800

    add integration spec test that failed

---
+++ spec/word_count_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+  require('./app')
+  Capybara.app = Sinatra::Application
+  set(:show_exceptions, false)
+
+  describe('the word-count score path', {:type => :feature}) do
+    it('processes the user entries and returns an evaluation of how many times a particular word is repeated in a sentence') do
+      visit('/')
+      fill_in('sentence', :with => "The dog was walking on the street while I was passing by eating a hot-dog.")
+      fill_in('word', :with => "dog")
+      click_button('Go')
+      expect(page).to have_content("The word 'dog' is repeated 1 time(s) in the sentence 'The dog was walking on the street while I was passing by eating a hot-dog.' and records a partial match 2 time(s).")
+    end
+  end
